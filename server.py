b0af9b3b41c77e9b0ed26fcf906808c12fd30e02
nkdxczh <zhc416@lehigh.edu>
2017-08-11 22:14:09 -0400

    support relative path

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
 import urllib2
+import os.path
+
+SERVER = "http://127.0.0.1:8081"
+PATH = "tem"
 
 class S(SimpleHTTPRequestHandler):
     def _set_headers(self):
@@ -11,23 +15,59 @@ class S(SimpleHTTPRequestHandler):
     def do_GET(self):
         print "get request %s" % (self.path)
         self._set_headers()
-        f = open(self.path[1:])
-        self.wfile.write(f.read())
-        f.close()
+
+        if os.path.isfile(self.path[1:]):
+            f = open(self.path[1:])
+            self.wfile.write(f.read())
+            f.close()
+        else:
+            file_name = self.path[len(PATH)+2:]
+            domains = file_name.split('/')
+            domain = domains[0].split(':')[0] + "://" + domains[0].split(':')[1]
+            for i in range(1,len(domains)):
+                domain += '/' + domains[i]
+            #download file
+            response = urllib2.urlopen(domain)
+            html = response.read()
+            f = open(PATH + '/' + file_name, 'w+')
+            f.write(html)
+            f.close()
+            f = open(PATH + '/' +file_name, 'r')
+            self.wfile.write(f.read())
+            f.close()
 
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
         print "post: " + post_body
+
+        #construct folder and index file name
+        domains = post_body.split('/')
+        folder = domains[0] + domains[2]
+        file_name = folder
+        if not os.path.exists(PATH+'/'+folder):
+            os.makedirs(PATH+'/'+folder)
+        for i in range(3, len(domains)):
+            file_name += '/' + domains[i]
+            if i < len(domains) - 1:
+                if not os.path.exists(PATH+'/'+file_name):
+                    os.makedirs(PATH+"/"+file_name)
+        if '.' not in domains[-1]:
+            if not os.path.exists(PATH+'/'+file_name):
+                os.makedirs(PATH+"/"+file_name)
+            file_name += "index.html"
+
+        #download file
         response = urllib2.urlopen(post_body)
         html = response.read()
-        file_name = post_body.split('/')[-1]
-        f = open('tem/' + file_name, 'w+')
+        f = open(PATH + '/' + file_name, 'w+')
         f.write(html)
-        f.write("console.log('inject!!!!')")
+        #f.write("console.log('inject!!!!')")
         f.close()
+
+        #send file position back
         self._set_headers()
-        self.wfile.write('http://127.0.0.1:8081/tem/'+file_name)
+        self.wfile.write(SERVER+"/"+PATH+"/"+file_name)
         #test_data = simplejson.loads(post_body)
         #print "post_body(%s)" % (test_data)
         #return SimpleHTTPRequestHandler.do_POST(self)
